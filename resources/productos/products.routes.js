aeee6f2e8f557a6a4ce48958c8ad498fff17d905
Luis <luis@email.com>
2019-10-11 21:34:43 -0500

    [ADD] CRUD & Product Validation

--- resources/productos/products.routes.js
+++ resources/productos/products.routes.js
@@ -1,12 +1,42 @@
 const express = require('express')
+const uuidv4 = require('uuid/v4');
 
-const products = require('../../db').products;
+
+const validateProduct = require('./products.validate');
+
+let products = require('../../db').products;
 
 const productsRoutes = express.Router()
 
 productsRoutes.get('/', (req, res) => {
-  console.log('GET IN PRODUCTS');
   res.json(products);
+});
+
+productsRoutes.post('/', validateProduct, (req, res) => {
+  const newProduct = { ...req.body, id: uuidv4() };
+  products.push(newProduct);
+  res.json(newProduct);
+})
+
+///products/098as908asd098asd089
+productsRoutes.put('/:id', (req, res) => {
+  const filterProduct = products.filter(product => product.id === req.params.id)[0];
+
+  const updatedProduct = { ...filterProduct, ...req.body  };
+
+  res.json(updatedProduct);
+})
+
+// DESTROY
+
+productsRoutes.delete('/:id', (req, res) => {
+  const filterProduct = products.filter(product => product.id === req.params.id)[0];
+
+  const productsWithoutSelected = products.filter(product => product.id !== req.params.id)[0];
+
+  products = productsWithoutSelected;
+
+  res.json(filterProduct);
 });
 
 
---
+++ resources/productos/products.validate.js
@@ -0,0 +1,20 @@
+const Joi = require('@hapi/joi')
+
+const productSchema = Joi.object({
+  name: Joi.string().min(2).max(100).required(),
+  price: Joi.number().min(0).required(),
+  currency: Joi.string().min(3).max(3).required()
+});
+
+
+const validateProduct = (req, res, next) => {
+  const validation = productSchema.validate(req.body);
+
+  if (validation.error) {
+    return res.status(403).send('Verifica tus datos')
+  }
+  next()
+}
+
+
+module.exports = validateProduct;

3da4ca1fe30f7768417dd66782a799719e95921d
Jhonny Neciosup Millones <jhonny.neciosup@indagia.com>
2019-10-13 18:41:07 -0500

    refactor(index.js): Clean the code

--- index.js
+++ index.js
@@ -1,45 +1,17 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-
 const productsRoutes = require('./resources/productos/products.routes');
-
 const app = express();
+const PORT = 4000;
 
-app.use(bodyParser.json())
+app.use(express.json())
+
+/* routes */
 app.use('/products', productsRoutes);
 
-
-
-/************************** */
-// READ
 app.get('/', (req, res) => {
-  res.status(200).send('Hola papu');
+  res.status(200).send('Welcome Krowders');
 });
 
-// CREATE
-app.post('/', (req, res) => {
-  console.log(req.body);
-  res.json(req.body);
-})
-
-// UPDATE
-app.put('/', () => {})
-
-// DESTROY
-app.delete('/', () => {})
-
-// CRUD
-// Create
-// Read
-// Update
-// Destroy
-
-
-const PORT = 3000;
-
 app.listen(PORT, () => {
-  console.log(`Nuestra app esta escuchando el puerto ${PORT}`);
+  console.log(`We app is listening on port: ${PORT}`);
 })
-
-
-console.log('Hola Mundo!')

02220996fcef69ac14fe03fd60229f52d2db1da3
Jhonny Neciosup Millones <jhonny.neciosup@indagia.com>
2019-10-15 21:28:18 -0500

    refactor(db): Repair database

--- db.js
+++ db.js
@@ -4,26 +4,26 @@ const products = [
     name: 'Media',
     price: 50.00,
     currency: 'PEN',
-    owner:1
+    owner:'1bf5g1hfg1h51gfh6'
   },
   {
     id: 'as09as9d0a9d0-a9',
     name: 'Pantalon',
     price: 150.00,
     currency: 'PEN',
-    owner:2
+    owner:'4jt4j4fg5j45g4j5g4'
   },
 ]
 
 const users = [{
   id: '1bf5g1hfg1h51gfh6',
   name: 'Antonio',
-  nikname: 'lymifa'
+  nickname: 'lymifa'
 },
 {
   id:'4jt4j4fg5j45g4j5g4',
   name: 'Jordy',
-  nikname: 'apple'
+  nickname: 'apple'
 }];
 
 // Trailing comma

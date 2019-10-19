7827c81e3284a9faa851837bb9495de97473b702
Luis <luis@email.com>
2019-10-18 20:35:30 -0500

    [ADD] logger

--- index.js
+++ index.js
@@ -1,14 +1,53 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const winston = require('winston');
+const morgan = require('morgan');
+
 const productsRoutes = require('./resources/productos/products.routes');
 
 const app = express();
 
+
 app.use(bodyParser.json())
+
+const logger = new (winston.Logger)({
+  transports: [
+    new winston.transports.File({
+      level: 'info',
+      json: false,
+      handleExceptions: true,
+      maxSize: 512000,
+      maxFiles: 5,
+      filename: `${__dirname}/log-de-aplicacion.log`,
+      prettyPrint: object => { return JSON.stringify(object) }
+    }),
+    new winston.transports.Console({
+      level: 'debug',
+      handleExceptions: true,
+      json: false,
+      colorize: true,
+      prettyPrint: object => { return JSON.stringify(object) }
+    })
+  ]
+})
+
+app.use(morgan('short', {
+  stream: {
+    write: message => logger.info(message.trim()),
+  }
+}));
+
 app.use('/products', productsRoutes);
 
 
+
+
+// SON EJEMPLOS
+// logger.log('log', 'Hello distributed log files!');
+// logger.info('info', 'Esto es un logger info');
+// logger.warn('WARN');
+// logger.error('ERROR');
 
 /************************** */
 // READ
@@ -40,6 +79,3 @@ const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Nuestra app esta escuchando el puerto ${PORT}`);
 })
-
-
-console.log('Hola Mundo!')

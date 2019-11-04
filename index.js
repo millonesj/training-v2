71080c4c25dceea7611a75abfa530a18682d672f
Jhonny Neciosup Millones <jhonny.neciosup@indagia.com>
2019-11-03 20:17:07 -0500

    refactor(index.js): order code

--- index.js
+++ index.js
@@ -1,38 +1,20 @@
 const express = require('express');
 const morgan = require('morgan');
-const passport = require('passport');
-const authJWT = require('passport-jwt');
 
 const config = require('./config')
-
-const jwtOptions = {
-  secretOrKey: 'SECRET_KEY',
-  jwtFromRequest: authJWT.ExtractJwt.fromAuthHeaderAsBearerToken()
-}
-
-let jwtStrategy = new authJWT.Strategy({ ...jwtOptions },(jwtPayload, next) => {
-  // usuarioDelPayLoad
-  console.log(jwtPayload)
-  next(null, {
-    id: jwtPayload.id
-  });
-})
-
-passport.use(jwtStrategy)
-
-const productsRoutes = require('./resources/productos/products.routes');
-const usersRoutes = require('./resources/users/users.routes');
-
 const {logger} = require('./resources/lib/logger');
 const auth = require('./resources/lib/authentication');
 const mongoose = require('mongoose')
 
+const productsRoutes = require('./resources/productos/products.routes');
+const usersRoutes = require('./resources/users/users.routes');
+
 const app = express();
-const PORT = 4000;
 
-app.use(express.json());
 
+/* DATABASE */
 mongoose.connect('mongodb://127.0.0.1:27017/training', { useNewUrlParser: true });
+
 mongoose.set('useFindAndModify', false);
 
 mongoose.connection.on('error', (error) => {
@@ -40,28 +22,21 @@ mongoose.connection.on('error', (error) => {
   process.exit(1);
 });
 
+
+/* MIDDLEWARES */
+app.use(express.json());
 app.use(morgan('{"remote_addr": ":remote-addr", "remote_user": ":remote-user", "date": ":date[clf]", "method": ":method", "url": ":url", "http_version": ":http-version", "status": ":status", "result_length": ":res[content-length]", "referrer": ":referrer", "user_agent": ":user-agent", "response_time": ":response-time"}', {
   stream: logger.stream}));
-
 app.use((req,res,next) => logger.saveParams(req,res,next));
 
-/* routes */
+
+/* ROUTES */
 app.use('/products', auth, productsRoutes);
 app.use('/users', usersRoutes);
 
 app.get('/', (req, res) => {
   res.status(200).send('Welcome Krowders');
 })
-
-// passport.use(new BasicStrategy((user, password, done) => {
-//   if (user === 'luis' && password === 'krowdy123') {
-//     return done(null, true);
-//   } else {
-//     return done(null, false);
-//   }
-// }))
-
-// app.use(passport.initialize())
 
 
 // SON EJEMPLOS
